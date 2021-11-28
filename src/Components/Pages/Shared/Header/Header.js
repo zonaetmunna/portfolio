b9d42e2fd376b97e8a13c357891ca34dbd483a37
zonaetmunna <zonaetmonna@gmail.com>
2021-11-29 05:18:43 +0600

    ad

--- src/Components/Pages/Shared/Header/Header.js
+++ src/Components/Pages/Shared/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
      return (
           <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                          <Nav className="me-auto">
