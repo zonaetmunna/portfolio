ea6556b4b386437cc1618077d0f9f0b7707cc1c0
zonaetmunna <zonaetmonna@gmail.com>
2021-12-04 19:02:37 +0600

    add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Components/Pages/Home/Home/Home';
+import SingleProject from './Components/Pages/Home/SingleProject/SingleProject';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/project/:id" element={<SingleProject></SingleProject>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/Components/Pages/Home/Project/Project.js
+++ src/Components/Pages/Home/Project/Project.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Card, Col, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
 
 const Project = ({ project }) => {
-     const { name, description, image } = project;
+     const { id, name, description, image } = project;
+     const navigate = useNavigate();
+
+     const handleDescription = () => {
+          navigate(`/project/${id}`);
+     }
+
      return (
 
           <Col>
                <Card>
-                    <Card.Img variant="top" src={image} />
+                    <Card.Img variant="top" src={image} width="100%" />
                     <Card.Body>
                          <Card.Title>{name}</Card.Title>
                          <Card.Text>
-                              {description.slice(0, 200)}
+                              {description.des1}
                          </Card.Text>
-                         <Button variant="primary">Description</Button>
+                         <Button onClick={handleDescription} variant="primary">Description</Button>
                     </Card.Body>
                </Card>
           </Col>
