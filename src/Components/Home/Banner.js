32bd786ec7f7d885b6f2f3532c080883edb52797
zonaetmunna <zonaetmonna@gmail.com>
2022-11-18 04:04:18 +0600

    banner design

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { Col, Container, Image, Row, Button } from "react-bootstrap";
+import { Col, Container, Image, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import pdf from "../../zonaet-hossain-resume-for-Frontend-developer.pdf";
+import { AiOutlinePhone } from "react-icons/ai";
+import { SiGmail } from "react-icons/si";
+import { BsFacebook, BsLinkedin, BsGithub } from "react-icons/bs";
 
 const Banner = () => {
   return (
@@ -9,22 +12,49 @@ const Banner = () => {
       <Container>
         <Row className="d-flex justify-center align-items-center">
           <Col sm={12} md={6}>
-            <h2>
-              Hello i am <span className="text-primary"> zonaet hossain</span>
-            </h2>
-            <h4 className="text-secondary">Front-end Developer</h4>
-            <h5>Phone : 01634501332</h5>
-            <h5>Email : zonaet.hossain.munna@gmail.com</h5>
-            <Button className="bg-dark">
-              <Link
-                className="text-decoration-none"
-                to={pdf}
-                target="_blank"
-                download
-              >
-                Resume
-              </Link>
-            </Button>
+            <h3>
+              Hello, I am{" "}
+              <span className="text-primary text-uppercase">
+                {" "}
+                zonaet hossain munna
+              </span>
+            </h3>
+            <p className="text-secondary">
+              I am a react web and mobile application developer. Focused mainly
+              on React. I love to build smooth and beautiful apps that solves
+              real-world problems!
+            </p>
+            <h6>
+              <span>
+                <AiOutlinePhone />
+              </span>
+              <span className="m-2">01634501332</span>
+            </h6>
+            <h6>
+              <span>
+                <SiGmail />
+              </span>
+              <span className="m-2">zonaet.hossain.munna@gmail.com</span>
+            </h6>
+            <h6>
+              <span>
+                <a href="https://github.com/zonaetmunna">
+                  <BsGithub />
+                </a>
+              </span>
+
+              <span className="m-2">
+                <a href="https://www.facebook.com/zonaet.monna/">
+                  <BsFacebook />
+                </a>
+              </span>
+
+              <span className="m-2">
+                <a href=" https://www.linkedin.com/in/md-zonaet-hossain-munna-22b909216/">
+                  <BsLinkedin />
+                </a>
+              </span>
+            </h6>
           </Col>
           <Col sm={12} md={6}>
             <Image
--- src/Components/common/Footer/Footer.js
+++ src/Components/common/Footer/Footer.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { AiOutlineCopyrightCircle } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>
-      <p>this is footer</p>
-    </div>
+    <Container
+      fluid
+      sticky="fixed"
+      className="d-flex justify-content-center algin-item-center bg-dark text-white"
+    >
+      <Row>
+        <Col>
+          <p>
+            copy right <AiOutlineCopyrightCircle />
+          </p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
